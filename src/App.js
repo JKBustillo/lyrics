8bdbdabdc4bac87dc9dda5cb4e16c5260d621ad5
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-10 16:15:01 -0500

    Canción component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
 
 function App() {
   const [busquedaLetra, setBusquedaLetra] = useState({});
@@ -23,6 +24,17 @@ function App() {
       <Formulario
         setBusquedaLetra={setBusquedaLetra}
       />
+
+      <div clasName="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            
+          </div>
+          <div className="col-md-6">
+            { letra ? <Cancion letra={letra}/> : null }
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
