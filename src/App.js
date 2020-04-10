ce1fa49737564686137fc709b61cf723dd02c243
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-10 13:33:59 -0500

    Form and lyrics in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+import Formulario from './components/Formulario';
 
 function App() {
+  const [busquedaLetra, setBusquedaLetra] = useState({});
+  const [letra, setLetra] = useState('');
+
+  useEffect(() => {
+    if (Object.keys(busquedaLetra).length === 0) return;
+    const consultarAPILetra = async () => {
+      const { artista, cancion } = busquedaLetra;
+      const url= `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+      const resultado = await axios(url);
+
+      setLetra(resultado.data.lyrics);
+    };
+    consultarAPILetra();
+  }, [busquedaLetra]);
   return (
     <Fragment>
-
+      <Formulario
+        setBusquedaLetra={setBusquedaLetra}
+      />
     </Fragment>
   );
 }
